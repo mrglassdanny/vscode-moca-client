33bc6d0cd0873d2d774d3203ca518cbe2d7b7f4e
dglass-longbow <dglass@longbowadvantage.com>
2020-10-07 23:14:26 -0500

    issue#1 fix

--- src/results/mocaResults.ts
+++ src/results/mocaResults.ts
@@ -18,22 +18,8 @@ export class MocaResults {
                 this.cols = [];
             }
 
-            const dateFormat = require('dateformat');
-
             if (data['results']['values']) {
                 this.rows = data['results']['values'] as Object[][];
-
-                // Checking for datetime type for formatting purposes.
-                for (var i = 0; i < this.cols.length; i++) {
-                    if (this.cols[i][1] === 'D') {
-                        for (var j = 0; j < this.rows.length; j++) {
-                            if (this.rows[j][i] !== null && this.rows[j][i] !== undefined) {
-                                var dateStr = new Date(this.rows[j][i].toString());
-                                this.rows[j][i] = dateFormat(dateStr, "isoDate") + "T" + dateFormat(dateStr, "isoTime");
-                            }
-                        }
-                    }
-                }
             } else {
                 this.rows = [];
             }
